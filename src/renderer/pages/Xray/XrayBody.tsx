d6d95911c140d5c5ca9ea0f057f770017e3e4d44
OPIUM66 <opiumdev@proton.me>
2024-10-17 12:04:35 +0330

    feat: add and delete xray into list

--- src/renderer/pages/Xray/XrayBody.tsx
+++ src/renderer/pages/Xray/XrayBody.tsx
@@ -6,102 +6,97 @@ import { cfFlag } from '../../lib/cfFlag';
 import XrayList from './XrayList';
 
 interface XrayBodyProps {
-  appLang: Language;
-  isConnected: boolean;
-  isLoading: boolean;
-  ipInfo: IpConfig;
-  ipData?: boolean;
-  proxyMode: string;
-  ping: number;
-  statusText: string;
-  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
-  handleOnSwipedLeft: () => void;
-  handleOnSwipedRight: () => void;
-  handleOnClickIp: () => void;
-  handleOnClickPing: () => void;
-  proxyStatus: string;
-  appVersion: string;
-  speeds: SpeedStats;
-  dataUsage: boolean;
+    appLang: Language;
+    isConnected: boolean;
+    isLoading: boolean;
+    ipInfo: IpConfig;
+    ipData?: boolean;
+    proxyMode: string;
+    ping: number;
+    statusText: string;
+    onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+    handleOnSwipedLeft: () => void;
+    handleOnSwipedRight: () => void;
+    handleOnClickIp: () => void;
+    handleOnClickPing: () => void;
+    proxyStatus: string;
+    appVersion: string;
+    speeds: SpeedStats;
+    dataUsage: boolean;
 }
 
 interface WelcomeMessageProps {
-  isConnected: boolean;
-  ipInfo: IpConfig;
-  statusText: string;
+    isConnected: boolean;
+    ipInfo: IpConfig;
+    statusText: string;
 }
 
-const WelcomeMessage: FC<WelcomeMessageProps> = ({
-  isConnected,
-  ipInfo,
-  statusText,
-}) => {
-  if (isConnected) {
-    return (
-      <div className="statusBar green">
-        <div className="box statusLabel">
-          <h4>{statusText}</h4>
+const WelcomeMessage: FC<WelcomeMessageProps> = ({ isConnected, ipInfo, statusText }) => {
+    if (isConnected) {
+        return (
+            <div className='statusBar green'>
+                <div className='box statusLabel'>
+                    <h4>{statusText}</h4>
 
-          {ipInfo.countryCode ? (
-            <p>
-              {ipInfo?.countryCode}:{ipInfo.ip || '127.0.0.1'}
-              <img
-                src={cfFlag(ipInfo.countryCode || 'xx')}
-                alt={`${ipInfo?.countryCode} Flag`}
-              />
-            </p>
-          ) : (
-            <></>
-          )}
-        </div>
-        <div className="box">
-          <i className="material-icons">check_circle</i>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="statusBar red">
-        <div className="box statusLabel">
-          <h4>{statusText}</h4>
-          <p> </p>
-        </div>
-        <div className="box">
-          <i className="material-icons">highlight_off</i>
-        </div>
-      </div>
-    );
-  }
+                    {ipInfo.countryCode ? (
+                        <p>
+                            {ipInfo?.countryCode}:{ipInfo.ip || '127.0.0.1'}
+                            <img
+                                src={cfFlag(ipInfo.countryCode || 'xx')}
+                                alt={`${ipInfo?.countryCode} Flag`}
+                            />
+                        </p>
+                    ) : (
+                        <></>
+                    )}
+                </div>
+                <div className='box'>
+                    <i className='material-icons'>check_circle</i>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className='statusBar red'>
+                <div className='box statusLabel'>
+                    <h4>{statusText}</h4>
+                    <p> </p>
+                </div>
+                <div className='box'>
+                    <i className='material-icons'>highlight_off</i>
+                </div>
+            </div>
+        );
+    }
 };
 
-
 const XrayBody: FC<XrayBodyProps> = ({
-  appLang,
-  handleOnClickIp,
-  handleOnClickPing,
-  handleOnSwipedLeft,
-  handleOnSwipedRight,
-  ipData,
-  ipInfo,
-  isConnected,
-  isLoading,
-  onSubmit,
-  ping,
-  proxyMode,
-  statusText,
-  proxyStatus,
-  appVersion,
-  speeds,
-  dataUsage,
+    appLang,
+    handleOnClickIp,
+    handleOnClickPing,
+    handleOnSwipedLeft,
+    handleOnSwipedRight,
+    ipData,
+    ipInfo,
+    isConnected,
+    isLoading,
+    onSubmit,
+    ping,
+    proxyMode,
+    statusText,
+    proxyStatus,
+    appVersion,
+    speeds,
+    dataUsage
 }) => {
-  return (
-    <div className={classNames('myApp', 'verticalAlign')}>
-      <div className="xrayScreen">
-        {WelcomeMessage({ isConnected, ipInfo, statusText })}
-        <div className="xrayList">
-        <XrayList  />
+    return (
+        <div className={classNames('myApp', 'verticalAlign')}>
+            <div className='xrayScreen'>
+                {WelcomeMessage({ isConnected, ipInfo, statusText })}
+                <div className='xrayList'>
+                    <XrayList />
 
-          {/* {isConnected ? (
+                    {/* {isConnected ? (
             <button onClick={onSubmit} className="green actionBtn">
               {appLang?.status?.connected}
             </button>
@@ -110,9 +105,9 @@ const XrayBody: FC<XrayBodyProps> = ({
               {appLang?.status?.disconnected}
             </button>
           )} */}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default XrayBody;
--- src/renderer/pages/Xray/XrayList.tsx
+++ src/renderer/pages/Xray/XrayList.tsx
@@ -3,75 +3,97 @@ import classNames from 'classnames';
 import { cfFlag } from '../../lib/cfFlag';
 
 interface XrayListProps {}
+interface handleAddTypes {
+  value: string;
+}
+
+interface Item {
+  id: string;
+  ip: string;
+  createdAt: number;
+  countryCode: string;
+}
 
 const XrayList: React.FC<XrayListProps> = () => {
   const [editingId, setEditingId] = useState<number | null>(null);
-  const [newIp, setNewIp] = useState<string>('');
+  const [Ips, setNewIp] = useState<Item[]>([]); // Array of Item objects
+  const [TempIp, setTempIp] = useState<string>('');
+
+  const generateRandomString = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+
+    for (let i = 0; i < 8; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length),
+      );
+      if (i === 3) result += ' '; // Add space after the fourth character
+    }
+
+    return result;
+  };
+
+  const addIp = (ip: string) => {
+    setNewIp((prevItems) => [
+      ...(prevItems || []),
+      {
+        id: generateRandomString(),
+        ip,
+        createdAt: new Date().getTime(),
+        countryCode: 'de',
+      },
+    ]);
+  };
 
   const handleAdd = () => {
-    setNewIp('');
-  };
-  const onDelete = () => {
-    setNewIp('');
+    const value = document.getElementById('ipInput')?.value;
+    if (value === '' || typeof value !== 'string' || !value.includes('vless')) {
+      console.log('value is not valid');
+      return;
+    }
+
+    addIp(value);
+    // setTempIp('')
   };
 
-  const handleUpdate = () => {
-    setEditingId(null);
+  const handleDelete = (id: string) => {
+    setNewIp((prevItems) => prevItems.filter(item => item.id !== id));
   };
-
+  
   return (
     <div className="crud-list">
       <div className="crud-input">
         <input
+          value={TempIp}
+          id="ipInput"
+          onChange={(e) => {
+            setTempIp(e.target.value);
+          }}
           type="text"
-          value={newIp}
-          onChange={(e) => setNewIp(e.target.value)}
           placeholder="vless:// , trojan:// , ssh:// , other"
         />
         <button className="actionBtn" onClick={handleAdd}>
-        <i className="material-icons">add</i>
+          <i className="material-icons">add</i>
         </button>
       </div>
 
       <ul className="crud-items">
-        {[
-          { id: 1, ip: '192.168.0.1', countryCode: 'us' },
-          { id: 2, ip: '172.16.0.1', countryCode: 'ca' },
-          { id: 3, ip: '10.0.0.1', countryCode: 'de' },
-        ].map((config) => (
+        {Ips.map((config) => (
           <li key={config.id} className="crud-item">
-            {editingId === config.id ? (
-              <input
-                type="text"
-                value={config.ip}
-                onChange={(e) =>
-                  handleUpdate({ ...config, ip: e.target.value })
-                }
+            <span>
+              {' '}
+              <img
+                src={cfFlag(config.countryCode)}
+                alt={`${config.countryCode} Flag`}
+                className="crud-flag"
               />
-            ) : (
-              <>
-                <span>
-                  {' '}
-                  <img
-                    src={cfFlag(config.countryCode)}
-                    alt={`${config.countryCode} Flag`}
-                    className="crud-flag"
-                  />
-                  {config.ip}
-                </span>
-              </>
-            )}
+              {config.ip}
+            </span>
             <div className="crud-actions">
-              <button
-                className="actionBtn"
-                onClick={() => setEditingId(config.id)}
-              >
+              <button className="actionBtn">
                 <i className="material-icons">edit</i>
               </button>
-              <button
-                className="actionBtn"
-                onClick={() => onDelete(config.id)}
-              >
+              <button className="actionBtn" onClick={() => handleDelete(config.id) }>
                 <i className="material-icons">close</i>
               </button>
             </div>
